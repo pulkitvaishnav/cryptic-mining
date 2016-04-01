82d473079bd1eee1cb4fa1cdfabd09d1a361c7cc
pulkitvaishnav <pulkitvaishnav35@gmail.com>
2016-04-01 09:35:56 +0530

    Graph is active now.

--- sparse_matrix/urls.py
+++ sparse_matrix/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 from . import views
-# from sparse_matrix.views import LineChartJSONView
+from sparse_matrix.views import LineChartJSONView
 
 urlpatterns = [
     # ex: /polls/
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r'^(?P<sparse_id>[0-9]+)/$', views.output, name='output'),
     url(r'^result/', views.result, name='result'),
     url(r'^sparse/', views.sparse, name='sparse'),
-    # url(r'^sparse_json$', LineChartJSONView.as_view(), name="line_chart_json"),
+    url(r'^sparse_json$', LineChartJSONView.as_view(), name="line_chart_json"),
     # ex: /polls/5/results/
     # url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
--- sparse_matrix/views.py
+++ sparse_matrix/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, render_to_response
 from django.views.decorators.csrf import csrf_exempt
 from random import randint
 from django.views.generic import TemplateView
-# from chartjs.views.lines import BaseLineChartView
+from chartjs.views.lines import BaseLineChartView
 import json
 
 from .models import Sparse
@@ -78,23 +78,34 @@ def sparse(request):
     return render_to_response('sparse_matrix/sparse.html', context_instance=RequestContext(request))
 
 
-# class LineChartJSONView(BaseLineChartView):
-#     def get_labels(self):
-#         """Return 7 labels."""
-#         latest_length_list = Sparse.objects.values(
-#             'length').order_by('-id')[:5]
-#         all_lengths = [p['length'] for p in latest_length_list]
-#         return all_lengths
+class LineChartJSONView(BaseLineChartView):
+    def get_labels(self):
+        """Return 7 labels."""
+        latest_length_list = Sparse.objects.values(
+            'length').order_by('-id')[:5]
+        all_lengths = [p['length'] for p in latest_length_list]
+        return all_lengths
 
-#     def get_data(self):
-#         """Return 3 datasets to plot."""
-#         latest_time_list = Sparse.objects.values(
-#             'processing_time_l').order_by('-id')[:5]
-#         all_processing_time = [p1['processing_time']
-#                                for p1 in latest_time_list]
-#         # import pdb; pdb.set_trace()
+    def get_data(self):
+        """Return 3 datasets to plot."""
+        latest_time_linear = Sparse.objects.values(
+            'processing_time_l').order_by('-length')[:15]
+        all_processing_linear = [p1['processing_time_l']
+                               for p1 in latest_time_linear]
+        
+        latest_time_quadratic = Sparse.objects.values(
+            'processing_time_q').order_by('-length')[:15]
+        all_processing_quadratic = [p1['processing_time_q']
+                               for p1 in latest_time_quadratic]
 
-#         return [all_processing_time]
+        latest_time_cubic = Sparse.objects.values(
+            'processing_time_c').order_by('-length')[:15]
+        all_processing_cubic = [p1['processing_time_c']
+                               for p1 in latest_time_cubic]
+        # import pdb; pdb.set_trace()
 
-#     line_chart = TemplateView.as_view(template_name='sparse.html')
-# line_chart_json = LineChartJSONView.as_view()
+        return [all_processing_linear,all_processing_quadratic, all_processing_cubic] 
+
+    
+line_chart = TemplateView.as_view(template_name='sparse.html')
+line_chart_json = LineChartJSONView.as_view()
